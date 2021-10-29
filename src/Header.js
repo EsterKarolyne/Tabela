c9506c320d06d52e94f0c06b55612b85d137cc5f
unknown <esterknappverly@gmail.com>
2021-10-29 10:12:46 -0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <nav>
+    <div className="nav-wrapper purple accent-3">
+      <a href="/" className="brand-logo">Casa do código</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="/autores">Autores</a></li>
+        <li><a href="/livros">Livros</a></li>
+        <li><a href="/precos">Preços</a></li>
+      </ul>
+    </div>
+  </nav>
+  );
+};
+
+export default Header;
